446c35bebeec81cf4db638bd83c97e9689eb6341
max <85299566+ThoXmax@users.noreply.github.com>
2021-09-09 15:29:41 +0800

    add: server side env file

--- server/index.js
+++ server/index.js
@@ -1,3 +1,17 @@
-//! cd ./server/
-//! npm init -y
-//! npm install bcrypt crypto dotenv express getstream stream-chat nodemon twilio
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
