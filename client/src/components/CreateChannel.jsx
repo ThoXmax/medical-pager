8e17563cb3b9126007220a0fcaccc8cd500c0159
max <85299566+ThoXmax@users.noreply.github.com>
2021-09-10 16:03:07 +0800

    feat: add channel name title

--- client/src/components/CreateChannel.jsx
+++ client/src/components/CreateChannel.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useChatContext } from 'stream-chat-react';
 
 import { UserList } from './';
@@ -21,10 +21,16 @@ const ChannelNameInput = ({ channelName = '', setChannelName }) => {
     )
 }
 
-const CreateChannel = () => {
+const CreateChannel = ({ createType, setIsCreating}) => {
+    const [channelName, setChannelName] = useState('');
+
     return (
-        <div>
-            <ChannelNameInput />
+        <div className="create-channel__container">
+            <div className="create-channel__header">
+                <p>{createType === 'team' ? 'Create a New Channel' : 'Send a Direct Message'}</p>
+                <CloseCreateChannel setIsCreating={setIsCreating} />
+            </div>
+            {createType === 'team' && <ChannelNameInput channelName={channelName} setChannelName={setChannelName} />}
         </div>
     )
 }
