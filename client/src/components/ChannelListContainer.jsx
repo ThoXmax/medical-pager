0a234dd6e801f3fd43e40d375ddc1db1c29b9fc8
max <85299566+ThoXmax@users.noreply.github.com>
2021-09-10 15:30:37 +0800

    feat: add Creat Channel button

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -29,7 +29,7 @@ const CompanyHeader = () => (
     </div>
 )
 
-const ChannelListContainer = () => {
+const ChannelListContainer = ({ isCreating, setIsCreating, setCreateType, setIsEditing }) => {
     const logout = () => {
         cookies.remove("token");
         cookies.remove('userId');
@@ -55,6 +55,11 @@ const ChannelListContainer = () => {
                         <TeamChannelList
                             {...listProps}
                             type="team"
+                            isCreating={isCreating}
+                            setIsCreating={setIsCreating}
+                            setCreateType={setCreateType}
+                            setIsEditing={setIsEditing} 
+
                         />
                     )}
                     Preview={(previewProps) => (
@@ -71,6 +76,11 @@ const ChannelListContainer = () => {
                         <TeamChannelList
                             {...listProps}
                             type="messaging"
+                            isCreating={isCreating}
+                            setIsCreating={setIsCreating}
+                            setCreateType={setCreateType}
+                            setIsEditing={setIsEditing} 
+
                         />
                     )}
                     Preview={(previewProps) => (
